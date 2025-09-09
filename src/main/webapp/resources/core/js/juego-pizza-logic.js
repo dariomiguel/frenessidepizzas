d92b9df5f3dbaea0c07793aaecd9495a98e493d9
Darío Miguel <dario.e.miguel@gmail.com>
2025-09-08 22:29:12 -0300

    Se agregaron ingredientes Albahaca, tomate y aceitunas

--- src/main/webapp/resources/core/js/juego-pizza-logic.js
+++ src/main/webapp/resources/core/js/juego-pizza-logic.js
@@ -2,6 +2,9 @@ let pedido = {
     masas: 0,
     salsas: 0,
     quesos: 0,
+    aceitunas:0,
+    albahacas:0,
+    tomates:0,
     lista: []
 };
 
@@ -9,11 +12,17 @@ let pedido = {
 const botonMasa = document.getElementById("botonMasa");
 const botonSalsa = document.getElementById("botonSalsa");
 const botonQueso = document.getElementById("botonQueso");
+const botonAlbahaca = document.getElementById("botonAlbahaca");
+const botonAceituna = document.getElementById("botonAceituna");
+const botonTomate = document.getElementById("botonTomate");
 const botonVerPedido = document.getElementById("entregarPedido");
 
 const cantidadDeMasa = document.getElementById("contadorMasas");
 const cantidadDeSalsa = document.getElementById("contadorSalsas");
 const cantidadDeQueso = document.getElementById("contadorQuesos");
+const cantidadDeAlbahaca = document.getElementById("contadorAlbahacas");
+const cantidadDeAceituna = document.getElementById("contadorAceitunas");
+const cantidadDeTomate = document.getElementById("contadorTomates");
 const resultadoPedido = document.getElementById("resultadoPedido");
 
 // Función genérica para actualizar
@@ -27,6 +36,9 @@ function agregarIngrediente(tipo, boton, contadorElemento) {
 botonMasa.addEventListener("click", () => agregarIngrediente("masas", botonMasa, cantidadDeMasa));
 botonSalsa.addEventListener("click", () => agregarIngrediente("salsas", botonSalsa, cantidadDeSalsa));
 botonQueso.addEventListener("click", () => agregarIngrediente("quesos", botonQueso, cantidadDeQueso));
+botonAlbahaca.addEventListener("click", () => agregarIngrediente("albahacas", botonAlbahaca, cantidadDeAlbahaca));
+botonAceituna.addEventListener("click", () => agregarIngrediente("aceitunas", botonAceituna, cantidadDeAceituna));
+botonTomate.addEventListener("click", () => agregarIngrediente("tomates", botonTomate, cantidadDeTomate));
 
 // Ver pedido
 botonVerPedido.addEventListener("click", () => {
