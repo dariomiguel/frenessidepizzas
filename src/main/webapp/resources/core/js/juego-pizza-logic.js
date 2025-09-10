1ac3f8b70fd15e0c8417bd39bbb5e71d939184e4
Darío Miguel <dario.e.miguel@gmail.com>
2025-09-09 22:53:40 -0300

    Intento de fusión con pruebas de svg

--- src/main/webapp/resources/core/js/juego-pizza-logic.js
+++ src/main/webapp/resources/core/js/juego-pizza-logic.js
@@ -1,4 +1,4 @@
-let pedido = {
+/*let pedido = {
     masas: 0,
     salsas: 0,
     quesos: 0,
@@ -45,4 +45,26 @@ botonVerPedido.addEventListener("click", () => {
     const pedidoFinal = pedido.lista.join(", ");
     console.log("Pedido:", pedidoFinal);
     resultadoPedido.textContent = `Tu pedido: ${pedidoFinal}`;
-});
+});*/
+
+function getName(elm)  {
+/*    let objectName = elm.getAttribute('name');
+    document.getElementById('debug').innerHTML= "name on hover: " + objectName;*/
+    console.log("hovered");
+};
+
+let selectedParts = [];
+
+function select(elm){
+    let partName = elm.getAttribute('name');
+
+    selectedParts.push(partName);
+    console.log(selectedParts)
+    let objectClass = elm.getAttribute('class');
+    document.getElementById('debug').innerHTML = "class on click: " + objectClass;
+
+
+    document.getElementById('selectedParts').innerHTML = "";
+    for (let i = 0; i < selectedParts.length; i++) {
+        document.getElementById('selectedParts').innerHTML += (selectedParts[i] + "<br>");}
+}
